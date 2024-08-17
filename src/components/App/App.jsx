b79775639f9e74e726d46f95cb3478b7cfff7ff2
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-08-17 23:11:58 +0300

    finish this homwork

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,57 @@
+import Description from "../Description/Description.jsx";
+import Feedback from "../Feedback/Feedback.jsx";
+import Notification from "../Notification/Notification .jsx";
+import Options from "../Options/Options.jsx";
+
+import { useState, useEffect } from "react";
 
 
-const App = () => {
+export default function App() {
+
+    const [value, setValue] = useState(() => {
+        const savedValue = window.localStorage.getItem('value');
+        return savedValue ? JSON.parse(savedValue) : { good: 0, neutral: 0, bad: 0 };
+        });
+
+    const updateFeedback = feedbackType => {
+        setValue({...value, [feedbackType]: value[feedbackType] + 1})
+    }
+
+    const totalFeedback = value.good + value.neutral + value.bad;
+
+    const positiveFeedback = totalFeedback
+    ? Math.round((value.good / totalFeedback) * 100)
+    : 0;
+
+    const resetFeedback = () => {
+        setValue({good: 0, neutral: 0, bad: 0})
+    }
+
+    useEffect(() => {
+        localStorage.setItem('value', JSON.stringify(value));
+        }, [value]);
+
+    return (
+        <>
+            <Description />
+
+            <Options 
+                updateFeedback={updateFeedback}
+                totalFeedback={totalFeedback}
+                resetFeedback={resetFeedback}
+                />
+
+            {totalFeedback === 0 ? (
+                <Notification />
+            ) : (
+                <Feedback 
+                good={value.good}
+                neatral={value.neutral}
+                bad={value.bad}
+                total={totalFeedback}
+                positive={positiveFeedback}
+            />
+            )}
+        </>
+    )
 };
-
-export default App;
---
+++ src/components/Notification/Notification .jsx
@@ -0,0 +1,9 @@
+// import css from "./Notification.module.css"
+
+export default function Notification() {
+    return (
+        <>
+            <h2>No feedback yet</h2>
+        </>
+    )
+}
